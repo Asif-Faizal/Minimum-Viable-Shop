90e9751a0ec244221d88daf65b6948d3943c24a2
asif <asif@loanwiser.in>
2026-01-16 12:23:55 +0530

    feat(account): add main application setup with retry logic
    Implement main application entry point for account service with database connection retry mechanism using github.com/tinrab/retry package. The setup includes configuration processing and gRPC server initialization.

--- account/cmd/account/main.go
+++ account/cmd/account/main.go
@@ -1,1 +1,36 @@
-package account
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/Asif-Faizal/Minimum-Viable-Shop/account"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/tinrab/retry"
+)
+
+type Config struct {
+	DatabaseUrl string `envconfig:"DATABASE_URL"`
+}
+
+func main() {
+	var config Config
+	err := envconfig.Process("", &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var repository account.Repository
+	retry.ForeverSleep(2*time.Second, func(_ int) (err error) {
+		repository, err = account.NewPostgresRepository(config.DatabaseUrl)
+		if err != nil {
+			log.Printf("failed to connect to database: %v", err)
+			return err
+		}
+		return nil
+	})
+	defer repository.Close()
+	log.Println("connected to database")
+	service := account.NewAccountService(repository)
+	log.Fatal(account.ListenGrpcServer(service, 8080))
+	log.Println("account service started")
+}
--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/sosodev/duration v1.3.1 // indirect
+	github.com/tinrab/retry v1.0.0 // indirect
 	golang.org/x/net v0.48.0 // indirect
 	golang.org/x/sys v0.40.0 // indirect
 	golang.org/x/text v0.33.0 // indirect
