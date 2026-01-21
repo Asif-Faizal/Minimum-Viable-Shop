d10d71f4f631d02bd9fce688cd86657ed717c1a2
asif <asif@loanwiser.in>
2026-01-21 20:33:34 +0530

    feat: Initialize order service gRPC server with database, account, and catalog clients.

--- order/cmd/order/main.go
+++ order/cmd/order/main.go
@@ -1,1 +1,38 @@
-package order
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/Asif-Faizal/Minimum-Viable-Shop/order"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/tinrab/retry"
+)
+
+type Config struct {
+	DatabaseUrl string `envconfig:"DATABASE_URL"`
+	AccountUrl  string `envconfig:"ACCOUNT_SERVICES_URL"`
+	CatalogUrl  string `envconfig:"CATALOG_SERVICES_URL"`
+}
+
+func main() {
+	var config Config
+	err := envconfig.Process("", &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var repository order.Repository
+	retry.ForeverSleep(2*time.Second, func(_ int) (err error) {
+		repository, err = order.NewPostgresRepository(config.DatabaseUrl)
+		if err != nil {
+			log.Printf("failed to connect to database: %v", err)
+			return err
+		}
+		return nil
+	})
+	defer repository.Close()
+	log.Println("connected to database")
+	service := order.NewOrderService(repository)
+	log.Fatal(order.ListenGrpcServer(service, config.AccountUrl, config.CatalogUrl, 8080))
+	log.Println("order service started")
+}
--- order/server.go
+++ order/server.go
@@ -1,1 +1,42 @@
 package order
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/Asif-Faizal/Minimum-Viable-Shop/account"
+	"github.com/Asif-Faizal/Minimum-Viable-Shop/catalog"
+	"github.com/Asif-Faizal/Minimum-Viable-Shop/order/pb/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+type GrpcServer struct {
+	orderService  Service
+	accountClient *account.AccountClient
+	catalogClient *catalog.CatalogClient
+	pb.UnimplementedOrderServiceServer
+}
+
+func ListenGrpcServer(service Service, accountUrl string, catalogUrl string, port int) error {
+	accountClient, err := account.NewAccountClient(accountUrl)
+	if err != nil {
+		return err
+	}
+	catalogClient, err := catalog.NewCatalogClient(catalogUrl)
+	if err != nil {
+		accountClient.Close()
+		return err
+	}
+	grpcServer := grpc.NewServer()
+	server := &GrpcServer{orderService: service, accountClient: accountClient, catalogClient: catalogClient}
+	pb.RegisterOrderServiceServer(grpcServer, server)
+	reflection.Register(grpcServer)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		accountClient.Close()
+		catalogClient.Close()
+		return err
+	}
+	return grpcServer.Serve(lis)
+}
